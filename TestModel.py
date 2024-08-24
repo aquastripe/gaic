cf6ce54a49dc800a5bfd6f9aa1cba45d985d63e3
Hao-Ting Li <maniac@csie.io>
2024-08-24 21:40:48 +0800

    feat: add no_rod for TestModel.py

--- TestModel.py
+++ TestModel.py
@@ -1,7 +1,7 @@
 import argparse
-import os
 
 import torch
+from tabulate import tabulate
 from torch.utils.data import DataLoader
 
 from TrainModel import collate_gaicd, test
@@ -15,6 +15,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Grid anchor based image cropping')
     parser.add_argument('--dataset_root', default='dataset/GAIC/', help='Dataset root directory path')
     parser.add_argument('--base_model', default='mobilenetv2', help='Pretrained base model')
+    parser.add_argument('--no_rod', action='store_true', default=False, help='No RoD Align')
     parser.add_argument('--params', default=None, help='Model parameters for testing')
     parser.add_argument('--scale', default='multi', type=str, help='choose single or multi scale')
     parser.add_argument('--downsample', default=4, type=int, help='downsample time')
@@ -35,18 +36,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-    args.save_folder = args.save_folder + args.base_model + '/' + 'downsample' + str(
-        args.downsample) + '_' + args.scale + '_Aug' + str(args.augmentation) + '_Align' + str(
-        args.align_size) + '_Cdim' + str(args.reduced_dim)
-
-    if not os.path.exists(args.save_folder):
-        os.makedirs(args.save_folder)
 
     test_set = GAICD(image_size=args.image_size, dataset_dir=args.dataset_root, set='test')
     data_loader_test = DataLoader(test_set, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False,
                                   persistent_workers=True, collate_fn=collate_gaicd)
     net = build_crop_model(scale=args.scale, alignsize=args.align_size, reddim=args.reduced_dim, loadweight=True,
-                           model=args.base_model, downsample=args.downsample)
+                           model=args.base_model, downsample=args.downsample, no_rod=args.no_rod)
     params = torch.load(args.params, map_location='cpu')
     net.load_state_dict(params)
 
@@ -57,11 +52,29 @@ def main():
         net = net.cuda()
 
     acc4_5, acc4_10, avg_srcc, avg_pcc, test_avg_loss, wacc4_5, wacc4_10 = test(net, data_loader_test)
-    print('[Test Loss: {:.4f}] '
-          '[{:.3f}, {:.3f}, {:.3f}, {:.3f}] '
-          '[{:.3f}, {:.3f}, {:.3f}, {:.3f}] '
-          '[SRCC: {:.3f}] [PCC: {:.3f}]'.format(test_avg_loss, *acc4_5, *acc4_10, avg_srcc, avg_pcc))
-    print('[{:.3f}, {:.3f}, {:.3f}, {:.3f}] [{:.3f}, {:.3f}, {:.3f}, {:.3f}]'.format(*wacc4_5, *wacc4_10))
+    results = (list(map(lambda x: f'{x:.1f}', [*acc4_5, *acc4_10])) +
+               list(map(lambda x: f'{x:.3f}', [avg_srcc, avg_pcc, test_avg_loss])))
+    table = tabulate(
+        [results],
+        headers=[
+            'Acc 1/5', 'Acc 2/5', 'Acc 3/5', 'Acc 4/5',
+            'Acc 1/10', 'Acc 2/10', 'Acc 3/10', 'Acc 4/10',
+            'SRCC', 'PLCC', 'Loss'
+        ],
+        tablefmt='orgtbl',
+    )
+    print(table)
+
+    results = list(map(lambda x: f'{x:.1f}', [*wacc4_5, *wacc4_10]))
+    table = tabulate(
+        [results],
+        headers=[
+            'WAcc 1/5', 'WAcc 2/5', 'WAcc 3/5', 'WAcc 4/5',
+            'WAcc 1/10', 'WAcc 2/10', 'WAcc 3/10', 'WAcc 4/10',
+        ],
+        tablefmt='orgtbl',
+    )
+    print(table)
 
 
 if __name__ == '__main__':
