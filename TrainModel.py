464ffe7a2d81ce274998655c3b20054d7b8b2e11
Hao-Ting Li <maniac@csie.io>
2024-08-24 12:36:37 +0800

    refactor: move code to the main function

--- TrainModel.py
+++ TrainModel.py
@@ -19,32 +19,26 @@ torch.manual_seed(SEED)
 np.random.seed(SEED)
 random.seed(SEED)
 
-parser = argparse.ArgumentParser(description='Grid anchor based image cropping')
-parser.add_argument('--dataset_root', default='dataset/GAIC/', help='Dataset root directory path')
-parser.add_argument('--base_model', default='mobilenetv2', help='Pretrained base model')
-parser.add_argument('--scale', default='multi', type=str, help='choose single or multi scale')
-parser.add_argument('--downsample', default=4, type=int, help='downsample time')
-parser.add_argument('--augmentation', default=1, type=int, help='choose single or multi scale')
-parser.add_argument('--image_size', default=256, type=int, help='Batch size for training')
-parser.add_argument('--align_size', default=9, type=int, help='Spatial size of RoIAlign and RoDAlign')
-parser.add_argument('--reduced_dim', default=8, type=int, help='Spatial size of RoIAlign and RoDAlign')
-parser.add_argument('--batch_size', default=1, type=int, help='Batch size for training')
-parser.add_argument('--resume', default=None, type=str, help='Checkpoint state_dict file to resume training from')
-parser.add_argument('--start_iter', default=0, type=int, help='Resume training at this iter')
-parser.add_argument('--num_workers', default=0, type=int, help='Number of workers used in dataloading')
-parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float, help='initial learning rate')
-parser.add_argument('--save_folder', default='weights/ablation/cropping/',
-                    help='Directory for saving checkpoint models')
-args = parser.parse_args()
 
-args.save_folder = args.save_folder + args.base_model + '/' + 'downsample' + str(
-    args.downsample) + '_' + args.scale + '_Aug' + str(args.augmentation) + '_Align' + str(
-    args.align_size) + '_Cdim' + str(args.reduced_dim)
-
-if not os.path.exists(args.save_folder):
-    os.makedirs(args.save_folder)
-
-cuda = True if torch.cuda.is_available() else False
+def parse_args():
+    parser = argparse.ArgumentParser(description='Grid anchor based image cropping')
+    parser.add_argument('--dataset_root', default='dataset/GAIC/', help='Dataset root directory path')
+    parser.add_argument('--base_model', default='mobilenetv2', help='Pretrained base model')
+    parser.add_argument('--scale', default='multi', type=str, help='choose single or multi scale')
+    parser.add_argument('--downsample', default=4, type=int, help='downsample time')
+    parser.add_argument('--augmentation', default=1, type=int, help='choose single or multi scale')
+    parser.add_argument('--image_size', default=256, type=int, help='Batch size for training')
+    parser.add_argument('--align_size', default=9, type=int, help='Spatial size of RoIAlign and RoDAlign')
+    parser.add_argument('--reduced_dim', default=8, type=int, help='Spatial size of RoIAlign and RoDAlign')
+    parser.add_argument('--batch_size', default=1, type=int, help='Batch size for training')
+    parser.add_argument('--resume', default=None, type=str, help='Checkpoint state_dict file to resume training from')
+    parser.add_argument('--start_iter', default=0, type=int, help='Resume training at this iter')
+    parser.add_argument('--num_workers', default=0, type=int, help='Number of workers used in dataloading')
+    parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float, help='initial learning rate')
+    parser.add_argument('--save_folder', default='weights/ablation/cropping/',
+                        help='Directory for saving checkpoint models')
+    args = parser.parse_args()
+    return args
 
 
 def collate_gaicd(batch):
@@ -67,29 +61,9 @@ def collate_gaicd(batch):
     return results
 
 
-train_set = GAICD(image_size=args.image_size, dataset_dir=args.dataset_root, set='train',
-                  augmentation=args.augmentation)
-data_loader_train = DataLoader(train_set, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True,
-                               worker_init_fn=random.seed(SEED), persistent_workers=True, collate_fn=collate_gaicd)
-test_set = GAICD(image_size=args.image_size, dataset_dir=args.dataset_root, set='test')
-data_loader_test = DataLoader(test_set, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False,
-                              persistent_workers=True, collate_fn=collate_gaicd)
-
-net = build_crop_model(scale=args.scale, alignsize=args.align_size, reddim=args.reduced_dim, loadweight=True,
-                       model=args.base_model, downsample=args.downsample)
-
-if cuda:
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    # cudnn.benchmark = True
-    net = net.cuda()
-
-optimizer = optim.Adam(net.parameters(), lr=args.lr)
-
-
 @torch.no_grad()
-def test():
-    net.eval()
+def test(model, data_loader_test):
+    model.eval()
     acc4_5 = []
     acc4_10 = []
     wacc4_5 = []
@@ -115,13 +89,13 @@ def test():
         roi = torch.tensor(roi).float()
         MOS = torch.tensor(MOS)
 
-        if cuda:
+        if torch.cuda.is_available():
             image = image.to('cuda')
             roi = roi.to('cuda')
             MOS = MOS.to('cuda')
 
         # t0 = time.time()
-        out = net(image, roi)
+        out = model(image, roi)
         loss = torch.nn.SmoothL1Loss(reduction='elementwise_mean')(out.squeeze(), MOS)
         total_loss += loss.item()
         avg_loss = total_loss / (id + 1)
@@ -176,8 +150,8 @@ def test():
     return acc4_5, acc4_10, avg_srcc, avg_pcc, avg_loss, wacc4_5, wacc4_10
 
 
-def train():
-    net.train()
+def train(model, data_loader_train, data_loader_test, optimizer, args):
+    model.train()
     for epoch in range(0, 80):
         total_loss = 0
         for id, sample in enumerate(data_loader_train):
@@ -196,14 +170,13 @@ def train():
             roi = torch.tensor(roi).float()
             MOS = torch.tensor(MOS)
 
-            if cuda:
+            if torch.cuda.is_available():
                 image = image.to('cuda')
                 roi = roi.to('cuda')
                 MOS = MOS.to('cuda')
 
             # forward
-
-            out = net(image, roi)
+            out = model(image, roi)
             loss = torch.nn.SmoothL1Loss(reduction='elementwise_mean')(out.squeeze(), MOS)
             total_loss += loss.item()
             avg_loss = total_loss / (id + 1)
@@ -216,7 +189,7 @@ def train():
             sys.stdout.write(
                 '\r[Epoch %d/%d] [Batch %d/%d] [Train Loss: %.4f]' % (epoch, 79, id, len(data_loader_train), avg_loss))
 
-        acc4_5, acc4_10, avg_srcc, avg_pcc, test_avg_loss, wacc4_5, wacc4_10 = test()
+        acc4_5, acc4_10, avg_srcc, avg_pcc, test_avg_loss, wacc4_5, wacc4_10 = test(model, data_loader_test)
         sys.stdout.write(
             '[Test Loss: %.4f] [%.3f, %.3f, %.3f, %.3f] [%.3f, %.3f, %.3f, %.3f] [SRCC: %.3f] [PCC: %.3f]\n' % (
                 test_avg_loss, acc4_5[0], acc4_5[1], acc4_5[2], acc4_5[3], acc4_10[0], acc4_10[1], acc4_10[2],
@@ -225,8 +198,39 @@ def train():
         sys.stdout.write('[%.3f, %.3f, %.3f, %.3f] [%.3f, %.3f, %.3f, %.3f]\n' % (
             wacc4_5[0], wacc4_5[1], wacc4_5[2], wacc4_5[3], wacc4_10[0], wacc4_10[1], wacc4_10[2], wacc4_10[3]))
         file_path = f'{args.save_folder}/{epoch:02d}_{avg_srcc:.3f}.pth'
-        torch.save(net.state_dict(), file_path)
+        torch.save(model.state_dict(), file_path)
+
+
+def main():
+    args = parse_args()
+    args.save_folder = args.save_folder + args.base_model + '/' + 'downsample' + str(
+        args.downsample) + '_' + args.scale + '_Aug' + str(args.augmentation) + '_Align' + str(
+        args.align_size) + '_Cdim' + str(args.reduced_dim)
+
+    if not os.path.exists(args.save_folder):
+        os.makedirs(args.save_folder)
+
+    train_set = GAICD(image_size=args.image_size, dataset_dir=args.dataset_root, set='train',
+                      augmentation=args.augmentation)
+    data_loader_train = DataLoader(train_set, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True,
+                                   worker_init_fn=random.seed(SEED), persistent_workers=True, collate_fn=collate_gaicd)
+    test_set = GAICD(image_size=args.image_size, dataset_dir=args.dataset_root, set='test')
+    data_loader_test = DataLoader(test_set, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False,
+                                  persistent_workers=True, collate_fn=collate_gaicd)
+
+    net = build_crop_model(scale=args.scale, alignsize=args.align_size, reddim=args.reduced_dim, loadweight=True,
+                           model=args.base_model, downsample=args.downsample)
+
+    if torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        # cudnn.benchmark = True
+        net = net.cuda()
+
+    optimizer = optim.Adam(net.parameters(), lr=args.lr)
+
+    train(net, data_loader_train, data_loader_test, optimizer, args)
 
 
 if __name__ == '__main__':
-    train()
+    main()
